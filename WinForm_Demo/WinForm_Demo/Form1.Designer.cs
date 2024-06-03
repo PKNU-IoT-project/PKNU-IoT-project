67f8d0e9de0c450141ab780ce2471d8e50efd7bd
breadcoffee <printf0543@gmail.com>
2024-06-03 21:52:57 +0900

    라즈베리파이 exe 테스트

---
+++ WinForm_Demo/WinForm_Demo/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace WinForm_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.RS232C = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Location = new System.Drawing.Point(266, 160);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(189, 106);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "Click this";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.btnClickThis_Click);
+            // 
+            // RS232C
+            // 
+            this.RS232C.PortName = "/dev/serial0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.IO.Ports.SerialPort RS232C;
+    }
+}
+
