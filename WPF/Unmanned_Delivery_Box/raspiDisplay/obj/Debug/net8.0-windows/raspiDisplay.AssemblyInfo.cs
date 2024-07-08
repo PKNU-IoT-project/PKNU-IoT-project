a6449780807effe60ac6e094f1feba7e1bd41756
guswlrla <rlaguswl_01@naver.com>
2024-07-08 13:32:35 +0900

    update

--- WPF/Unmanned_Delivery_Box/raspiDisplay/obj/Debug/net8.0-windows/raspiDisplay.AssemblyInfo.cs
+++ WPF/Unmanned_Delivery_Box/raspiDisplay/obj/Debug/net8.0-windows/raspiDisplay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("raspiDisplay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28a8c2a67ccf1eb648b4953b65ce550cafc20eb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbae26305bccfd7c099efebf65e5a37449e9d6ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("raspiDisplay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("raspiDisplay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WPF/Unmanned_Delivery_Box/raspiDisplay/receiver.xaml.cs
+++ WPF/Unmanned_Delivery_Box/raspiDisplay/receiver.xaml.cs
@@ -25,94 +25,39 @@ namespace raspiDisplay
             InitializeComponent();
         }
 
-        private void AppendTextToTextBox(string text)
-        {
-            this.NumTxtBox_2.Text += text;
-            NumTxtBox_2.Focus();
-            NumTxtBox_2.CaretIndex = NumTxtBox_2.Text.Length; // 커서를 맨 뒤로 이동
-        }
-
-        private void backBtn_2_Click(object sender, RoutedEventArgs e)
+        private void backBtn_Click(object sender, RoutedEventArgs e)
         {
             raspiDisplay.userType usertype = new raspiDisplay.userType();
             usertype.Show();
             this.Close();
         }
 
-        private void homeBtn_2_Click(object sender, RoutedEventArgs e)
+        private void homeBtn_Click(object sender, RoutedEventArgs e)
         {
             raspiDisplay.MainWindow main = new raspiDisplay.MainWindow();
             main.Show();
             this.Close();
         }
 
-        private void Btn1_2_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            AppendTextToTextBox("1");
-        }
-
-        private void Btn2_2_Click(object sender, RoutedEventArgs e)
-        {
-            AppendTextToTextBox("2");
-        }
-
-        private void Btn3_2_Click(object sender, RoutedEventArgs e)
-        {
-            AppendTextToTextBox("3");                   
-        }
-
-        private void Btn4_2_Click(object sender, RoutedEventArgs e)
-        {
-            AppendTextToTextBox("4");
-        }
-
-        private void Btn5_2_Click(object sender, RoutedEventArgs e)
-        {
-            AppendTextToTextBox("5");
-        }
-
-        private void Btn6_2_Click(object sender, RoutedEventArgs e)
-        {
-            AppendTextToTextBox("6");
-        }
-
-        private void Btn7_2_Click(object sender, RoutedEventArgs e)
-        {
-            AppendTextToTextBox("7");
-        }
-
-        private void Btn8_2_Click(object sender, RoutedEventArgs e)
-        {
-            AppendTextToTextBox("8");
-        }
-
-        private void Btn9_2_Click(object sender, RoutedEventArgs e)
-        {
-            AppendTextToTextBox("9");
-        }
-
-        private void Btn11_2_Click(object sender, RoutedEventArgs e)
-        {
-            AppendTextToTextBox("0");
-        }
-
-        private void Btn10_2_Click(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(this.NumTxtBox_2.Text)) 
+            if (sender is Button button && button.Tag is string number)
             {
-                this.NumTxtBox_2.Text = this.NumTxtBox_2.Text.Substring(0, this.NumTxtBox_2.Text.Length - 1);
-                NumTxtBox_2.Focus();
-                NumTxtBox_2.CaretIndex = NumTxtBox_2.Text.Length;
+                NumTxtBox.Text += number;
+                NumTxtBox.Focus();
+                NumTxtBox.CaretIndex = NumTxtBox.Text.Length; // 포커스 뒤로 가져오기
             }
         }
 
-        // 버튼을 누르면 텍스트박스 활성화가 안됨..;;
-        private void NumTxtBox_2_TextChanged(object sender, TextChangedEventArgs e)
+        private void Btn10_Click(object sender, RoutedEventArgs e)
         {
-            if (NumTxtBox_2.Text.Length > 6)
+            if (!string.IsNullOrEmpty(this.NumTxtBox.Text))
             {
-                NumTxtBox_2.Text = NumTxtBox_2.Text.Substring(0, 6);
+                this.NumTxtBox.Text = this.NumTxtBox.Text.Substring(0, this.NumTxtBox.Text.Length - 1);
+                NumTxtBox.Focus();
+                NumTxtBox.CaretIndex = NumTxtBox.Text.Length;
             }
         }
+
     }
 }
